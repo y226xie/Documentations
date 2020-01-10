9b6ec49020c6af0988471be11e4c32055d50f52d
Wendy Xie <wendy.xie@sonova.com>
2020-01-10 14:52:47 -0500

    finished data type notes for python

--- 7. Python Notes/DataType.py
+++ 7. Python Notes/DataType.py
@@ -5,13 +5,16 @@ print(type(x))
 # String
 a = 'Hello'
 
-# int 
+# int, or integer, is a whole number, positive or negative, without decimals, of unlimited length
 b = 20
 
-# float
+# float, or floating point number is a number, positive or negative, containing one or more decimals
+# float can also be scientific numbers with an "e" to indicate the power of 10
 c = 20.5
+c = 12E4
+c = 87.7e100
 
-# complex
+# complex, complex numbers are written with a "j" as the imaginary part
 d = 1j
 
 # list(array??)
@@ -26,7 +29,7 @@ g = range(6)
 # dict
 h = {"name": "John", "age": 36}
 
-# set 
+# set
 i = {"apple", "banana", "cherry"}
 
 # frozenset
@@ -35,7 +38,7 @@ l = frozenset({"apple", "banana", "cherry"})
 # boolean
 m = True
 
-# bytes 
+# bytes
 n = b"Hello"
 
 # bytearray
@@ -43,3 +46,15 @@ o = bytearray(5) # bytesarray(b'\x00\x00\x00\x00\x00')
 
 # memoryview
 p = memoryview(bytee(5)) # memory at 0x013D8FA0
+
+# type conversion
+q =  1
+q = float(q)
+q = complex(q)
+
+# Random Number
+# Python does not have a random() function to make a random number, but Python has a built-in module called `random` that can be used to make random numbers
+
+import random
+print(random.randrange(1, 10))
+
