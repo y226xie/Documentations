85b47c990a3dc240a118c1562f36cb4d2d7df5bf
Wendy Xie <35283396+y226xie@users.noreply.github.com>
2020-01-13 10:41:22 -0500

    Add files via upload

---
+++ 7. Python Notes/Operators.py
@@ -0,0 +1,43 @@
+# Operators
+
+# Arithmetic operators
+# +,-,*,/,%, **(exponentiation), //(floor division)
+
+# Comparison Operators
+# ==, !=, >, < , >=, <=
+
+# Python Logical Operators
+# and operator
+x < 5 and x < 10
+# or operator
+x < 5 or x < 4
+# not operator
+not (x < 5 and x < 10)
+
+# Python identity Operators
+# Identity operators are used to compare the objects, not if they are equal but if they are actually the same object, with the same memory loction
+
+# is  - returns true if both variables are the same object
+x is y
+
+# is not  - returns true if both variables are not the same object
+x is not y
+
+# Python membership Operators
+# in - returns True if a sequence with the specified value is present in the object
+x in y
+
+# not in - returns True if a sequence with the specified value is not present in the object
+x not in y
+
+
+# Python Bitwise Operators
+# Bitwise operators are used to compare(binary) numbers:
+
+# & - AND - Sets each bit to 1 if both bits are 1
+# | - OR  - Sets each bit to 1 if one of two bits is 1
+# ^ - XOR - Sets each bit to 1 if if only one of two bits is 1
+# ~ - NOT
+# << - shift left
+# >> - shift right
+
